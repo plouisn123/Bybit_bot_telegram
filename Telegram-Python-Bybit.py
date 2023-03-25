006d3cf6f5a12fd7e098bd4152ed74c7f838c78c
Rakece <111010001+plouisn123@users.noreply.github.com>
2023-03-25 21:12:17 +0100

    Update Telegram-Python-Bybit.py

--- Telegram-Python-Bybit.py
+++ Telegram-Python-Bybit.py
@@ -1,94 +1,47 @@
 import requests
 import ccxt
+import time
 
+##Telegram_bot
 exchange = ccxt.bybit({
-    'apiKey': 'API_KEY',
-    'secret': 'SECRET_API_KEY',
+    'apiKey': 'YOUR_API_KEY',
+    'secret': 'YOUR_SECRET_API_KEY'
 })
 
-bot_token = 'BOT_TOKEN'
-channel_id = '@CHANEL'
-last_update_id = 0
-dollar=float(50) #choose how many usdt from your wallet you want to trade
+bot_token = 'YOUR_BOT_TOKEN'
+channel_id = '@YOUR_CHANEL'
 
-#Get new messages from Telegram Chanel
+
+##Take message from Telegram
+last_update_id = 0
+dico  = {}
 while True:
-    response = requests.get(f"https://api.telegram.org/botBOT_TOKEN/getUpdates?offset={last_update_id+1}&allowed_updates=[\"channel_post\"]").json()
+    response = requests.get(f"https://api.telegram.org/botYOUR_BOT_TOKEN/getUpdates?offset={last_update_id+1}&allowed_updates=[\"channel_post\"]").json()
     updates = response["result"]
     for update in updates:
-        message = updates[-1]["channel_post"]["text"]
-        #message = update["channel_post"]["text"]
-        last_update_id = update["update_id"] 
-        text = message
+        if "channel_post" in update and "text" in update["channel_post"]:
+            message = update["channel_post"]["text"]
+            text = message
+            last_update_id = update["update_id"]
+        else:
+            text = ''
         
-        #extract data from brut order
-        if "TP :" in text and "SL :" in text and "Prix" in text:
+        ##Extraction of data
+        if "TEXT_CONDITION_TO_BE_SURE_THAT'S_A_ORDER" in text and exchange.fetch_balance()['USDT']['free'] > 10: #condition to deteminate values
             
-            #LONG or SHORT
-            start = text.find("(")
-            end = text.find(")", start)
-            if start != -1 and end != -1:
-                L_S = text[start+1:end]
-                BorS = 'Buy' if L_S == 'LONG' else 'Sell'
-                close = 'Buy' if BorS == 'Sell' else 'Sell'
-                print('Side:',BorS)
-                #print(type(BorS))
-                #print(type(close))
-                
-            #Pair    
-            start = text.find("")
-            end = text.find("(", start)
-            if start != -1 and end != -1:
-                crypto = text[start+1:end-1]
-                symbol = crypto+'USDT'
-                print('Paire:',symbol)
-                #print(type(symbol))
-                #print(type(crypto))
-
-            #Entry price number 1                 
-            start = text.find("entré")
-            end = text.find("-", start)
-            if start != -1 and end != -1:
-                PE11 = text[start+8:end]
-                PE1 = float(PE11.replace(",", "."))
-                #print('Entry Price 1:',PE1)
-
-            #Enter Price number 2
-            start = text.find("-")
-            end = text.find(" ", start)
-            if start != -1 and end != -1:
-                PE22 = text[start+1:end-3]
-                PE2 = float(PE22.replace(",", "."))
-                #print('Entry Price 2:',PE2)
-                #print(type(PE2))
-
-            #Take Profit number 1   
-            start = text.find("TP")
-            end = text.find(" SL", start)
-            if start != -1 and end != -1:
-                TP = text[start+6:end-3]        
-                TPs = TP.split("\n")
-                TPs[1] = float(TPs[1].replace(",", "."))
-                print('Take Profit:',TPs[1])
-                #print(type(TPs[1]))
-
-            #Stop Loss  
-            start = text.find("SL")
-            end = text.find("Chaque", start)
-            if start != -1 and end != -1:
-                SLL = text[start+5:end-2]
-                SL = float(SLL.replace(",", "."))
-                print('Stop Loss:',SL)
-                #print(type(SL))
-                
-            #Set the arguments
-            #Entry price
-            last_price = exchange.fetch_ticker(symbol)['last']
-            nb_decimals = len(str(last_price)) - str(last_price).index('.') - 1
+            PE1 = 'ENTRY PRICE NUMBER 1' #float
+            PE2 = 'ENTRY PRICE NUMBER 2' #float
+            SL = 'STOP LOSS' #float
+            TP = 'TAKE PROFIT' #float
+            PAIRE = 'BTCUSDT' #str
+            
+            
+            #Entry Price
+            last_price = exchange.fetch_ticker(symbol)['last'] # take the last price 
+            nb_decimals = len(str(last_price)) - str(last_price).index('.') - 1 #find the number of decimals
             index = str(last_price).find('.')
-            #print('last price: ',last_price)
             if PE1 < last_price < PE2:
-                if index == -1: 
+                if index == -1: #est ce que le prix de la crypto à une virgule ?
                     PE = round(last_price*1.001)
                     print('PE:',PE)
                 else:
@@ -103,9 +56,8 @@ while True:
                 PE = PE1
                 print('PE:',PE)
 
-            #Leverage
-            
-            lev_max = exchange.fetch_derivatives_market_leverage_tiers(symbol)[0]['maxLeverage'] #maximum leverage            
+            #choix levier
+            lev_max = exchange.fetch_market_leverage_tiers(symbol)[0]['maxLeverage'] #levier maximum
             if 0.1 < last_price < 100:
                 if lev_max < 20:
                     levier = lev_max
@@ -127,50 +79,137 @@ while True:
                 else:
                     levier = 35
             print('Levier',levier)
-
-            #Place order
-            if symbol is not None and BorS is not None and PE is not None and close is not None and TPs[1] is not None and SL is not None and levier is not None:
-                info_leviers=exchange.fetch_positions(symbol) #get infos about the pair (actual leverage and minimal quantity)
-                size = info_leviers[0]['info']['size']
-                #Set Leverage 
+            
+            levier = float(10)
+            ##Passage des ordres
+            if all(var in globals() for var in ['BorS', 'PE', 'close', 'TPs', 'SL', 'levier']):
+                info_leviers=exchange.fetch_positions(symbol) #récupère le gros tas d'info sur la paire
+                size = info_leviers[0]['info']['size'] #taille de l'ordre long en court (recuperer nb décimals)
+                #leviers
                 if len(info_leviers) == 1:
                     levier_LS = info_leviers[0]['leverage']
                 else:
-                    levier_long = info_leviers[0]['leverage']
-                    levier_short = info_leviers[1]['leverage']
-                if levier_LS is not None:
-                    if levier != levier_LS:
-                        exchange.set_leverage(symbol=symbol, leverage=levier)
-                else:
+                    levier_long = info_leviers[0]['leverage'] 
+                    levier_short = info_leviers[1]['leverage'] 
+                try: 
+                    if levier_LS not in globals():
+                        if levier != levier_LS:
+                            exchange.set_leverage(symbol=symbol, leverage=levier)
+                except: 
                     if BorS == 'Buy':
                         if levier != levier_long:
                             exchange.set_leverage(symbol=symbol, leverage=levier)
                     else:
                         if levier != levier_short:
                             exchange.set_leverage(symbol=symbol, leverage=levier)
-                            
-                #Quantity
+                           
+                #calcul quantity
                 if "." in size:
                     decimals = size.split(".")[1]
                     nb_decimals = decimals.count("0")
-                    quantity = round(dollar*levier/last_price,nb_decimals)
+                    quantity = round(levier/last_price,nb_decimals)
                     if quantity == 0:
                         quantity = 1/10**nb_decimals
                 else:
-                    quantity = round(dollar*levier/last_price)
+                    quantity = round(levier/last_price)
                 print('Quantity',quantity)
                 
-                #Orders
-                exchange.create_order(symbol=symbol, type='Limit', side=BorS, amount=quantity, price=PE)            #Entry
-                exchange.create_order(symbol=symbol, type='Limit', side=close, amount=quantity, price=TPs[1])       #TP 
+                ## Ordres
+                #Entrer
+                orderPE = exchange.create_limit_order(symbol=symbol, side=BorS, amount=quantity, price=PE)
+    
+                #TP
+                orderTP = exchange.create_order(symbol=symbol, type='limit', side=close, amount=quantity, price=TPs[2])                    
+                #SL
                 if BorS == 'Buy':
-                    exchange.create_limit_order(symbol=symbol, side=close, amount=quantity, price=SL, params={'stopLossPrice': SL}) #SL close long
+                    orderSL = exchange.create_limit_order(symbol=symbol, side=close, amount=quantity, price=SL, params={'stopLossPrice': SL}) #Close long
                 else:
-                    exchange.create_limit_buy_order(symbol=symbol, amount=quantity, price=SL, params = {'stopPrice': SL}) #SL (short)
-                    
-#Problem 1: if liquidation price > stop loss -> stop loss order become short order
-#Problem 2: if take profit is hit, stop loss not desactivate -> short order
+                    orderSL = exchange.create_limit_buy_order(symbol=symbol, amount=quantity, price=SL, params = {'stopPrice': SL}) #Open long
+        
+                #ajouter les ordres au dico
+                dico[symbol]=[]
+                dico[symbol].append(orderPE['id']), dico[symbol].append(orderTP['id']), dico[symbol].append(orderSL['id'])
+
+                time.sleep(5)
+          
+    ## Gestion des ordes 
+    order_book = exchange.fetch_derivatives_open_orders() #liste des ordres à executer
+    if len(order_book) != 0:
+        symb = []
+        for i in range(len(order_book)):
+            symb.append(order_book[i]['info']['symbol']) #nom des paires de tous les ordres dans une liste
+
+        # nombre d'ordres par paire
+        dico_actif = {}
+        for item in symb:
+            if item in dico_actif:
+                dico_actif[item] += 1
+            else:
+                dico_actif[item] = 1
+        liste_actif = list(dico_actif.items()) #nombre d'ordres par paire en liste
+        #print(liste_actif)
+
+        #récupération des paires de trades en cours
+        trade_actif = exchange.fetch_derivatives_positions()
+        paire_active = []
+        if len(trade_actif) != 0:
+            paire_active = []
+            for i in range(len(trade_actif)):
+                paire_active.append(trade_actif[i]['info']['symbol'])
+        
+        # cloture des trades inutiles + suppression ID dans le dico
+        for i in range(len(dico)):
+            if liste_actif[i][1] == 2: # vérifier si le trade n'a pas été fermé ou liquidé quand 2 ordres sont ouverts
+                #quel ordre est fermé ? 
+                fpe = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][0], symbol=list(dico.keys())[i])
+                ftp = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][1], symbol=list(dico.keys())[i])
+                fsl = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][2], symbol=list(dico.keys())[i])
+                #print('222',fpe, ftp, fsl)
+                if list(dico.keys())[i] not in paire_active: #paire du dico des ordres pas dans les paires en cours de trade ? 
+                    if fpe != 'open':
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][1], symbol=list(dico.keys())[i])
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][2], symbol=list(dico.keys())[i])
+                        del dico[list(dico.keys())[i]]
+                        print(dico)
+                    elif ftp != 'open':
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][0], symbol=list(dico.keys())[i])
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][2], symbol=list(dico.keys())[i])
+                        del dico[list(dico.keys())[i]]
+                    elif fsl != 'open':
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][0], symbol=list(dico.keys())[i])
+                        exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][1], symbol=list(dico.keys())[i])
+                        del dico[list(dico.keys())[i]]
+                
+            if liste_actif[i][1] == 1: # vérifier si le trade n'a pas été fermé ou liquidé quand 1 ordres sont ouverts
+                #quel ordre est fermé ? 
+                fpe = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][0], symbol=list(dico.keys())[i])
+                ftp = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][1], symbol=list(dico.keys())[i])
+                fsl = exchange.fetch_order_status(id = dico[list(dico.keys())[i]][2], symbol=list(dico.keys())[i])
+                #print('111',fpe, ftp, fsl)
+                if (fpe == 'closed' or fpe == 'canceled') and (ftp == 'closed' or ftp =='canceled'):
+                    exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][2], symbol=list(dico.keys())[i])
+                    del dico[list(dico.keys())[i]]
+                elif (fsl == 'closed' or fsl == 'canceled') and (fpe == 'closed' or fpe =='canceled'):
+                    exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][1], symbol=list(dico.keys())[i])
+                    del dico[list(dico.keys())[i]]
+                elif (fsl == 'closed' or fsl == 'canceled') and (ftp == 'closed' or ftp =='canceled'):
+                    exchange.cancel_derivatives_order(id= dico[list(dico.keys())[i]][0], symbol=list(dico.keys())[i])
+                    del dico[list(dico.keys())[i]]
+        time.sleep(4)
+    
+    #print(dico)
+  
+
+
+
+    
+
+                
+                
+
+
+ 
+
 
         
-               
-        
+            
